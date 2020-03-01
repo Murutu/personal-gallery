5a56da262dc4df33a6ef5d6f40cebb47bb1b9bfa
Murutu <murutupeter@email.com>
2020-03-01 15:29:33 +0300

    added def gallery_of_day  in views.py

--- gallery/views.py
+++ gallery/views.py
@@ -27,3 +27,17 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def news_of_day(request):
+    date = dt.date.today()
+
+    # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
+    day = convert_dates(date)
+    html = f'''
+        <html>
+            <body>
+                <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+        </html>
+            '''
+    return HttpResponse(html)
