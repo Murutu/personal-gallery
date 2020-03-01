5fa2c84f64f53c1cfe24ec5d4b40f38bf64a634d
Murutu <murutupeter@email.com>
2020-03-01 15:47:14 +0300

    added except value error   in views.py

--- gallery/views.py
+++ gallery/views.py
@@ -43,6 +43,18 @@ def gallery_of_day(request):
     return HttpResponse(html)
 
 def past_days_gallery(request,past_date):
+    
+    def past_days_news(request,past_date):
+    
+        try:
+            # Converts data from the string Url
+            date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+        except ValueError:
+            # Raise 404 error when ValueError is thrown
+            raise Http404()
+    
+    
         # Converts data from the string Url
         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
 
