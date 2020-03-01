b479b8f8612ad0e501aa4725145beb73ed67120a
Murutu <murutupeter@email.com>
2020-03-01 15:42:54 +0300

    added import Http404   in views.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns=[
     path('today/',views.gallery_today,name='galleryToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_news,name = 'pastNews'),  
+
 ]
--- gallery/views.py
+++ gallery/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
 import datetime as dt
 
 # Create your views here.
@@ -28,7 +28,7 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def news_of_day(request):
+def gallery_of_day(request):
     date = dt.date.today()
 
     # FUNCTION TO CONVERT DATE OBJECT TO FIND EXACT DAY
@@ -41,3 +41,17 @@ def news_of_day(request):
         </html>
             '''
     return HttpResponse(html)
+
+def past_days_gallery(request,past_date):
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+
+        day = convert_dates(date)
+        html = f'''
+            <html>
+                <body>
+                    <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+                </body>
+            </html>
+                '''
+        return HttpResponse(html)
