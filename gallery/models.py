67dd5596f2d30ecf1e41716e31d61f2d69d26089
Murutu <murutupeter@email.com>
2020-03-02 06:59:15 +0300

    added def copy image in models.py

--- gallery/models.py
+++ gallery/models.py
@@ -2,8 +2,33 @@ from django.db import models
 
 # Create your models here.
 class Image(models.Model):
-    image_title = models.CharField(max_length=30)
+    image_title = models.CharField(max_length=60)
     image_description = models.TextField(max_length=500)
+    image_category = models.CharField(max_length=30,blank=True)
+    
+    def __str__(self):
+        return self.image_title
+    
+    def save_image(self):
+        self.save()
+        
+    class Meta:
+        ordering = ['image_title']
+        
+    def update_image(self,image_title,image_category=None):
+        self.image_title=image_title if image_title else self.image_category
+        
+    @classmethod
+    def all_images(cls):
+        images = cls.objects.all()
+        return images
+    
+    @classmethod
+    def search_by_category(cls,category):
+        images = cls.objects.filter(image_category__icontains=category)
+        return images
+    
+        
     
 class tags(models.Model):
     name = models.CharField(max_length =30)
--- gallery/urls.py
+++ gallery/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns=[
     path('today/',views.gallery_today,name='galleryToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastNews'),  
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery'),
+    url(r'^image/(\d+)',views.image,name='image')  
 
 ]
