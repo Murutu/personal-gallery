f510ee6a8c0a3265ba7450ff445e164263273c5a
Murutu <murutupeter@email.com>
2020-03-02 09:22:37 +0300

    added a url for search in urls.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns=[
     path('today/',views.gallery_today,name='galleryToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_gallery,name = 'pastGallery'),
-    url(r'^image/(\d+)',views.image,name='image')  
+    url(r'^image/(\d+)',views.image,name='image'),
+    url(r'^search/', views.search_results, name='search_results') 
 
 ]
